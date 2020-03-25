4f9c488a2cc5d70524539d8ac5f92854e03448d4
jameshess9 <jshess@buffalo.edu>
2020-03-24 22:27:32 -0400

    Changing from localhost to tethys

--- config.php
+++ config.php
@@ -1,10 +1,10 @@
 <?php
 /* Database credentials. Assuming you are running MySQL
 server with default setting (user 'root' with no password) */
-define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
-define('DB_NAME', 'cse442_proj');
+define('DB_SERVER', 'tethys.cse.buffalo.edu');
+define('DB_USERNAME', 'jshess');
+define('DB_PASSWORD', '50185428');
+define('DB_NAME', 'cse442_542_2020_spring_teaml_db');
  
 /* Attempt to connect to MySQL database */
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
