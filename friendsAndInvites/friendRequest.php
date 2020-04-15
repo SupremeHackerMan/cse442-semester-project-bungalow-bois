ed42c49d503866454e0990abb42bc7f289735b3f
Jackie Ling <jling2@buffalo.edu>
2020-04-15 15:04:20 -0400

    debug-fixed a friend request bug where you could send the request to yourself

--- friendsAndInvites/friendRequest.php
+++ friendsAndInvites/friendRequest.php
@@ -13,14 +13,29 @@
 
     $conn = new mysqli($HOST, $USERNAME, $USERPASSWORD, $DBNAME);
 
-    //checks if a request was already sent                                    
-    $result = $conn->query("SELECT * FROM `FriendRequests` WHERE (`requester` = '$currentUserName' AND  `requestee` = '$friendo' )");
+    /*
+        Rules
+        -wont send a request if request already sent
+        -wont send a request if requestee sent you a request
+        -wont send a request if you sent a request to your self 
+        -wont send a request if you guys are already friends
+    */
+
+    //checks if a request was already sent either by you or (the other person to you)                                    
+    $alreadySent1 = $conn->query("SELECT * FROM `FriendRequests` WHERE (`requester` = '$currentUserName' AND  `requestee` = '$friendo' )");
+    $alreadySent2 = $conn->query("SELECT * FROM `FriendRequests` WHERE (`requester` = '$friendo' AND  `requestee` = '$currentUserName' )");
+    $alreadySent =($alreadySent1->num_rows + $alreadySent2->num_rows ) == 0;
 
     //checks if that user even exists
-    $result2 = $conn->query("SELECT * FROM `users` WHERE `username` = '$friendo' ");
+    $doesExist = $conn->query("SELECT * FROM `users` WHERE `username` = '$friendo' ");
+
+    //checks if already friends
+    $alreadyFriends1 = $conn->query("SELECT * FROM `Friends` WHERE (`friend1Username` = '$currentUserName' AND  `Friend2Username` = '$friendo' )");
+    $alreadyFriends2 = $conn->query("SELECT * FROM `Friends` WHERE (`friend1Username` = '$friendo' AND  `Friend2Username` = '$currentUserName' )");
+    $alreadyFriends =($alreadyFriends1->num_rows + $alreadyFriends2->num_rows ) == 0;
     
-    //if no entry found then insert
-    if ($result->num_rows == 0 && $result2->num_rows != 0) {
+    
+    if ($alreadySent &&  $doesExist->num_rows != 0 && $friendo != $currentUserName && $alreadyFriends) {
         $sqlQuery2 = "INSERT INTO `FriendRequests` (`requester`,  `requestee`) 
                                         VALUES ('$currentUserName', '$friendo') ";    
         $conn->query($sqlQuery2);                               
