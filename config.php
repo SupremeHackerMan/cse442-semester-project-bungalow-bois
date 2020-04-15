97762bd37eb9d7d6120626ee9a6df54d0d9aed14
jameshess9 <jshess@buffalo.edu>
2020-04-15 12:58:36 -0400

    Revert "Revert "Merge branch 'develop'""
    This reverts commit f7ff953bff76ea6f9135257e38a1f39b5f6be647.

--- config.php
+++ config.php
@@ -5,10 +5,10 @@ define('DB_SERVER', 'tethys.cse.buffalo.edu');
 define('DB_USERNAME', 'jshess');
 define('DB_PASSWORD', '50185428');
 define('DB_NAME', 'cse442_542_2020_spring_teaml_db');
- 
+
 /* Attempt to connect to MySQL database */
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
- 
+
 // Check connection
 if($link === false){
     die("ERROR: Could not connect. " . mysqli_connect_error());
